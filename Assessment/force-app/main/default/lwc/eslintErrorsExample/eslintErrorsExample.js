56e1ebef0801f6a9cdb6674fceaf0d952099f6b8
Parvathi <parvathi_pampana@persistent.com>
2023-04-12 16:26:41 +0530

    mySecondCommit

---
+++ Assessment/force-app/main/default/lwc/eslintErrorsExample/eslintErrorsExample.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+//import getAccounts from '@salesforce/apex/DemoCombobox.getAccounts';
+export default class EslintErrorsExample extends LightningElement {
+
+}
